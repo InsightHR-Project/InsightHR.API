0c8fab7e777c7879acaf2fd5a46fe0b4640fedb1
Prankrishnakk <prankrishnakk340@gmail.com>
2025-08-23 21:16:48 +0530

    roleservices

---
+++ InsightHR.API/Controller/RoleController.cs
@@ -0,0 +1,66 @@
+﻿
+using InsightHR.Application.Dtos;
+using InsightHR.Application.Interfaces;
+using InsightHR.Shared.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsightHR.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IRoleService _roleService;
+
+        public RoleController(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+
+        // GET: api/Role
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _roleService.GetAll();
+            return StatusCode(response.StatusCode, response);
+        }
+
+        // GET: api/Role/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var response = await _roleService.GetById(id);
+            if (!response.Success)
+                return NotFound(response);
+
+            return Ok(response);
+        }
+
+        // POST: api/Role
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] RoleDto role)
+        {
+            var response = await _roleService.Create(role);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        // PUT: api/Role
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] RoleUpdateDto role)
+        {
+            var response = await _roleService.Update(role);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        // DELETE: api/Role/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _roleService.Delete(id);
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+
+}
+
---
+++ InsightHR.Application/Dtos/RoleDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsightHR.Application.Dtos
+{
+    public class RoleDto
+    {
+        public string? Name { get; set; }   
+    }
+
+    public class RoleUpdateDto 
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
