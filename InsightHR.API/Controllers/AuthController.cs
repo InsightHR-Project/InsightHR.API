2d621aec4af80111c8220763900989c30279d671
Prankrishnakk <prankrishnakk340@gmail.com>
2025-08-23 10:09:34 +0530

    INITIAldifjr

---
+++ InsightHR.API/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsightHR.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("AuthController is working!");
+        }
+    }
+}
